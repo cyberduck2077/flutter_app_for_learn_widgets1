f59cd12f87e952aa0a29868bb25df370a0454e4c
Sergey Utka <1sergeyutka1@gmail.com>
2022-10-27 13:08:15 +0300

    added UserBloc

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_for_learn_widgets1/blocs/generated_bloc.dart';
+import 'package:flutter_app_for_learn_widgets1/blocs/user_bloc/user_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,101 +13,124 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<GeneratedBloc>(
-      create: (context) => GeneratedBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<GeneratedBloc>(
+          create: (context) => GeneratedBloc(),
+        ),
+        BlocProvider<UserBloc>(
+          create: (context) => UserBloc(),
+        ),
+      ],
       child: BlocBuilder<GeneratedBloc, int>(
         builder: (context, state) {
           return Scaffold(
-              floatingActionButton: FloatingActionButton(
-                onPressed: () => BlocProvider.of<GeneratedBloc>(context)
-                    .add(CounterDecEvent()),
-                child: Icon(Icons.exposure_minus_1),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => BlocProvider.of<GeneratedBloc>(context)
+                  .add(CounterDecEvent()),
+              child: Icon(Icons.exposure_minus_1),
+            ),
+            backgroundColor: Colors.white,
+            body: SafeArea(
+              minimum: const EdgeInsets.all(8),
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PageStateWidget(),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      UserBlocWidget(),
+                    ],
+                  ),
+                  // InputTextWidget(),
+                  ButtonNextState(),
+                ],
               ),
-              backgroundColor: Colors.white,
-              body: SafeArea(
-                minimum: const EdgeInsets.all(8),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.black26,
-                            border:
-                                Border.all(width: 2, color: Colors.blueAccent)),
-                        child: Text(
-                          BlocProvider.of<GeneratedBloc>(context)
-                              .state
-                              .toString(),
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.blueAccent,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                    // InputTextWidget(),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: TextButton(
-                        onPressed: () => BlocProvider.of<GeneratedBloc>(context)
-                            .add(CounterIncEvent()),
-                        child: Text(
-                          "Increment value".toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.green,
-                              fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ));
+            ),
+          );
         },
       ),
     );
   }
 }
 
-// class PageStateWidget extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//           alignment: Alignment.center,
-//           height: 180,
-//           decoration: BoxDecoration(
-//               shape: BoxShape.circle,
-//               color: Colors.black26,
-//               border: Border.all(width: 2, color: Colors.blueAccent)),
-//           child: Text(
-//             "",
-//             style: TextStyle(
-//               fontSize: 24,
-//               color: Colors.blueAccent,
-//               fontWeight: FontWeight.w600,
-//             ),
-//           )),
-//     );
-//   }
-// }
+class PageStateWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        height: 180,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black26,
+            border: Border.all(width: 2, color: Colors.blueAccent)),
+        child: BlocBuilder<GeneratedBloc, int>(
+          builder: (context, state) {
+            return Text(
+              BlocProvider.of<GeneratedBloc>(context).state.toString(),
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w600,
+              ),
+            );
+          },
+        ));
+  }
+}
 
-// class ButtonNextState extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Align(
-//       alignment: Alignment.bottomCenter,
-//       child: TextButton(
-//         onPressed: () {},
-//         child: Text(
-//           "Next state".toUpperCase(),
-//           style: TextStyle(fontSize: 16, color: Colors.green),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class UserBlocWidget extends StatelessWidget {
+  const UserBlocWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UserBloc, UserState>(
+      // bloc: UserBloc(),
+      builder: (context, state) {
+        return Column(
+          children: [
+            if (state is UserLoadingState) CircularProgressIndicator(),
+            if (state is UserLoadedState)
+              ...state.users.map(
+                  (e) => Text(e.name, style: TextStyle(color: Colors.green))),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ButtonNextState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          TextButton(
+            onPressed: () {
+              BlocProvider.of<GeneratedBloc>(context).add(CounterIncEvent());
+            },
+            child: Text(
+              "Next Generated State".toUpperCase(),
+              style: TextStyle(fontSize: 16, color: Colors.blueAccent.shade400),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              BlocProvider.of<UserBloc>(context).add(UserGetUsersEvent(
+                  BlocProvider.of<GeneratedBloc>(context).state));
+            },
+            child: Text(
+              "Next User State".toUpperCase(),
+              style: TextStyle(fontSize: 16, color: Colors.green),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
